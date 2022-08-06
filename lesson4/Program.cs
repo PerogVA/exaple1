fdcdbf7ea598913ef6f4595ce0eb43ad480798bc
Vadim <vadim-perog@mail.ru>
2022-08-06 11:16:16 +0300

    4

---
+++ lesson4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число  ");
+int num = Convert.ToInt32(Console.ReadLine ());
+if (num == 1) Console.Write("Пн");
+else if (num == 2) Console.Write("вт");
+else if (num == 3) Console.Write("ср");
+else if (num == 4) Console.Write("чт");
+else if (num == 5) Console.Write("пт");
+else if (num == 6) Console.Write("сб");
+else if (num == 7) Console.Write("вс");
+else Console.WriteLine("Вы ввели неправильное число ");
+
